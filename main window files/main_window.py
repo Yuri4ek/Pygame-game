5721972a7ce5e097f442cba5df3fb3357cf4cd15
Yuri4ek <dinourski@gmail.com>
2024-12-21 22:37:17 +0300

    Добавил начальный код главного окна

---
+++ main window files/main_window.py
@@ -0,0 +1,26 @@
+import pygame
+
+# взятие данных обьектов
+with open("objects coordinates.txt", mode="r") as file:
+    data = [l.split(";") for l in file.read().split("\n")]
+
+size = width, height = list(map(int, data[0][2].split(",")))
+
+# инициализация окна
+pygame.init()
+window = pygame.display.set_mode(size)
+pygame.display.set_caption("Главное окно")
+
+# добавление фона
+background_image = pygame.image.load('main_window.png')
+window.blit(background_image, (0, 0))
+pygame.display.flip()
+
+# запуск окна
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
