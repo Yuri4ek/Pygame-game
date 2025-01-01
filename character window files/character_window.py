40c9f5064bac437ed221a6ffaae04770d949e966
Yuri4ek <dinourski@gmail.com>
2025-01-01 16:46:01 +0300

    Рефакторинг

--- character window files/character_window.py
+++ character window files/character_window.py
@@ -3,51 +3,70 @@ from functions import load_image
 import os
 import subprocess
 
-# взятие данных обьектов
-with open("objects coordinates.txt", mode="r") as file:
-    data = [l.split(";") for l in file.read().split("\n")]
-character_view_coordinates = x1, y1, x2, y2 = list(map(int,
-                                                       data[1][1].split(",")
-                                                       )
-                                                   )
 
-size = width, height = list(map(int, data[0][2].split(",")))
+def get_coordinates():
+    # взятие данных обьектов
+    with open("objects coordinates.txt", mode="r") as file:
+        data = [l.split(";") for l in file.read().split("\n")]
 
-# инициализация окна
-pygame.init()
-window = pygame.display.set_mode(size)
-pygame.display.set_caption("Pump It Up💪💪💪")
+    # размер окна
+    size = width, height = list(map(int, data[0][2].split(",")))
 
-# добавление фона
-background_image = pygame.image.load('window.png')
-window.blit(background_image, (0, 0))
+    # координаты обьектов окна
+    character_view_coordinates = list(map(int, data[1][1].split(",")))
+    general_lvl_coordinates = list(map(int, data[2][1].split(",")))
+    other_lvl_coordinates = list(map(int, data[3][1].split(",")))
 
-all_sprites = pygame.sprite.Group()
-sprite = pygame.sprite.Sprite()
+    return (size, character_view_coordinates,
+            general_lvl_coordinates, other_lvl_coordinates)
 
-# обновление картинки персонажа
-subprocess.run(["python", "make total character.py"])
 
-# загрузка картинки персонажа
-current_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = 'total_character.png'
-sprite.image = load_image(file_path, character_view_coordinates)
+def make_character(character_view_coordinates):
+    x1, y1, x2, y2 = character_view_coordinates
 
-# помещение персонажа в прямоугольник
-sprite.rect = sprite.image.get_rect()
-sprite.rect.x, sprite.rect.y = x1, y1
+    sprite = pygame.sprite.Sprite()
 
-all_sprites.add(sprite)
-all_sprites.draw(window)
+    # обновление картинки персонажа
+    subprocess.run(["python", "make total character.py"])
 
-# обновление окна
-pygame.display.flip()
+    # загрузка картинки персонажа
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = 'total_character.png'
+    sprite.image = load_image(file_path, character_view_coordinates)
 
-# запуск окна
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    # помещение персонажа в прямоугольник
+    sprite.rect = sprite.image.get_rect()
+    sprite.rect.x, sprite.rect.y = x1, y1
 
-pygame.quit()
+    all_sprites.add(sprite)
+
+
+size, character_view_coordinates, \
+    general_lvl_coordinates, other_lvl_coordinates = get_coordinates()
+
+if __name__ == '__main__':
+    # инициализация окна
+    pygame.init()
+    window = pygame.display.set_mode(size)
+    pygame.display.set_caption("Pump It Up💪💪💪")
+
+    # добавление фона
+    background_image = pygame.image.load('window.png')
+    window.blit(background_image, (0, 0))
+
+    # вывод персонажа
+    all_sprites = pygame.sprite.Group()
+    make_character(character_view_coordinates)
+    all_sprites.draw(window)
+
+    # обновление окна
+    pygame.display.flip()
+
+    # запуск окна
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    pygame.quit()
