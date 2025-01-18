a15e3689df5c7767e5b2aa9b44a1d40d3f4e0c40
Yuri4ek <dinourski@gmail.com>
2025-01-18 19:37:43 +0300

    Удалил из requirements.txt ненужные библиотеки

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from mainWindowFiles.functions import *
 
 pygame.init()
-window = pygame.display.set_mode((800, 800))
+window = pygame.display.set_mode((600, 600))
 pygame.display.set_caption("Pump It Up💪💪💪")
 run_window(*get_coordinates())
 pygame.quit()
